8906d528d2b17a937ad306600f6c5b6236292c72
Dave Ceddia <dceddia@gmail.com>
2018-06-19 18:07:35 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => "Shopping Cart";
+
+ReactDOM.render(<App />, document.getElementById('root'));
