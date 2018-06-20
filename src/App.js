cb1ea67dda3b07e5f6712a6ff9526834ca9a1b90
Dave Ceddia <dceddia@gmail.com>
2018-06-19 21:00:41 -0400

    Add reach-router and basic routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ItemPage from './ItemPage';
 import CartPage from './CartPage';
+import { Link, Router } from '@reach/router';
 
 const App = () => (
   <div>
-    <ItemPage />
-    <CartPage />
+    <nav>
+      <Link to="/">Items</Link> <Link to="/cart">Cart</Link>
+    </nav>
+    <Router>
+      <ItemPage path="/" />
+      <CartPage path="/cart" />
+    </Router>
   </div>
 );
 
